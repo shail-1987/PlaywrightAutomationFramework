7c631c149ff98fc799224c73497af15e5ef0a7c2
Shailendra Prakash <prakashshailendra45@gmail.com>
2026-05-13 21:23:04 +0530

    initial commit

--- tests/practiceAnything.spec.ts
+++ tests/practiceAnything.spec.ts
@@ -1,10 +1,15 @@
-//create a simple arrow function that takes two numbers and returns their sum
-import {test} from '@playwright/test'
-test("practiceTest",()=>{
-let res=(a:number,b:number):number=>{
-    console.log("Using arrow function");
-return a+b;
-}
 
-console.log(res(6,7));
-})
+
+//var👇
+
+function demo() {
+    var name: string = "Atharv";
+    console.log("trying print");
+    console.log(name);
+    if (true) {
+        console.log(name);
+
+
+    }
+}
+demo();
