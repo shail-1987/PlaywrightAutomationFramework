9f9c5ad1004a58370455adcc57bbd551184bfe45
Shailendra Prakash <prakashshailendra45@gmail.com>
2026-05-19 19:49:38 +0530

    autotrigger5

--- tests/account_info.spec.ts
+++ tests/account_info.spec.ts
@@ -44,4 +44,4 @@ test.describe('@sanity Account information functional tests', () => {
     await expect(editAccountPage.continueButton).toBeVisible();
   });
 });
-//hbjkkkllnk
+//hbjkkkllnk_new changes
