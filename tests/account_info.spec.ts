91f0c33b6c657249c101c1628936ce7f682d2934
Shailendra Prakash <prakashshailendra45@gmail.com>
2026-05-27 17:38:11 +0530

    created google gropu and done chnges in account_info

--- tests/account_info.spec.ts
+++ tests/account_info.spec.ts
@@ -44,4 +44,4 @@ test.describe('@sanity Account information functional tests', () => {
     await expect(editAccountPage.continueButton).toBeVisible();
   });
 });
-//Changes should trigger pipeline anothet
+//Changes should trigger pipeline anothett
